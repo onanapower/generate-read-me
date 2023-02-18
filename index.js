05a1f9fa4a8a6b5f53aff6caec7b2dbe3fe85eb6
onanike obinna <onanikefrancis@gmail.com>
2023-02-18 04:38:50 +0000

    modified-packagejson-file-and-reinstalled-a-lower-npm-module

--- index.js
+++ index.js
@@ -1,12 +1,56 @@
-const inquirer = require("/inquirer");
+const inquirer = require("inquirer");
 const fs = require("fs");
 
+
+const propmtUser () =>
 inquirer
   .prompt([
     /* Pass your questions in here */
+    {
+      type: "input",
+      name: "Title",
+      message: "The title of the project?",
+    },
+    {
+      type: "input",
+      name: "Description",
+      message: "Kindly describe your project?",
+    },
+    {
+      type: "input",
+      name: "Ttableofcontents",
+      message: "List your table of contents",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "how can it be installed?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "How can you use it?",
+    },
+    {
+      type: "input",
+      name: "contributors",
+      message: "who are the contributors",
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "how can you test",
+    },
+    {
+      type: "input",
+      name: "questions",
+      message: "what are the frequently asked questions",
+    },
   ])
+
   .then((answers) => {
     // Use user feedback for... whatever!!
+    console.log(answers);
   })
   .catch((error) => {
     if (error.isTtyError) {
